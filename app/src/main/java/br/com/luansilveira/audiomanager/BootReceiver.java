4a48b19fc45e7b38c5e76459be89dcc654ea8b9e
luan-silveira <luan_silveira@outlook.com>
2020-02-20 00:58:41 -0300

    Ajustes HorarioManager

--- app/src/main/java/br/com/luansilveira/audiomanager/BootReceiver.java
+++ app/src/main/java/br/com/luansilveira/audiomanager/BootReceiver.java
@@ -3,11 +3,13 @@ package br.com.luansilveira.audiomanager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import br.com.luansilveira.audiomanager.db.DB;
 import br.com.luansilveira.audiomanager.db.Model.Horario;
@@ -17,9 +19,13 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-
         try {
             Dao<Horario, ?> dao = DB.get(context).getDao(Horario.class);
+            List<Horario> horarios = dao.queryForAll();
+            if (HorarioManager.from(context).isManagerAtivado()) {
+                HorarioManager.agendarHorarios(context, horarios);
+                Toast.makeText(context, "Horários Silencioso/Vibratório reagendados!", Toast.LENGTH_LONG).show();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
